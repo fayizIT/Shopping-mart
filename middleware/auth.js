e1731a8f07b7feaa7410ced35c03776dac4417aa
Fayiz <fayizcj94.com>
2023-11-16 17:42:16 +0530

    auth updates

--- middleware/auth.js
+++ middleware/auth.js
@@ -41,19 +41,10 @@ const isLogOut = async (req, res, next) => {
   }
 };
 
-const otpLog = async (req, res, next) => {
-  try {
-    if (req.session.user_id) {
-      res.redirect("/home");
-    }
-    next();
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+
 
 module.exports = {
   isLogin,
   isLogOut,
-  otpLog
+ 
 };
